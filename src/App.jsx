a49964fe5a2f9b2c00df1c7a3f7cb94b33ffbb0e
Wemelly Nunes <wemellyohanna@gmail.com>
2024-08-29 11:37:08 -0300

    feat: add o componente de input secondary

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import ButtonSecondary from "./components/buttons/buttonSecondary";
 import ButtonTertiary from "./components/buttons/buttonTertiary";
 import Header from "./components/header";
 import InputPrimary from "./components/inputs/inputPrimary";
+import InputSecondary from "./components/inputs/inputSecondary";
+import { FaEye, FaArrowRight, FaSearch } from 'react-icons/fa'; // Exemplo de ícones da biblioteca react-icons
+
 
 
 function App() {
@@ -13,6 +16,11 @@ function App() {
         <Header/>
         <p>ola</p>
         <InputPrimary label="Solicitante" placeholder="Informe" />
+        <InputSecondary 
+        label="Senha"
+        placeholder="Digite sua senha"
+        type="password"
+        buttonIcon={<FaEye/>}/>
         <ButtonPrimary>Primário</ButtonPrimary>
         <ButtonSecondary>Secundário</ButtonSecondary>
         <ButtonTertiary>Terciário</ButtonTertiary>
---
+++ src/components/inputs/inputSecondary/index.jsx
@@ -0,0 +1,27 @@
+const InputSecondary = ({ label, placeholder, buttonIcon, onButtonClick, type = 'text' }) => {
+    return (
+      <div className="w-full max-w-xs mb-4">
+        <label className="block text-primary-dark text-sm font-normal" htmlFor="inputWithButton">
+          {label}
+        </label>
+        <div className="relative flex items-center">
+          <input
+            className="appearance-none block w-full bg-white text-gray-700 border border-gray-300 
+            rounded h-10 px-4 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500
+            focus:border-blue-500 my-1 text-sm italic"
+            id="inputWithButton"
+            type={type}
+            placeholder={placeholder}
+          />
+          <button
+            onClick={onButtonClick}
+            className="absolute inset-y-0 right-0 flex items-center pr-3 text-blue-600 focus:outline-none "
+          >
+            {buttonIcon}
+          </button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default InputSecondary;
